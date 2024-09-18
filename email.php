8b76eb2c835ca219104d53b1198f98980903c8cb
TruongNDA <sdtla0911114819@gmail.com>
2024-09-18 15:11:01 +0700

    update code

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 $subject = 'You Got Message'; // Subject of your email
-$to = 'sdtla0911114819@gmail.com';  //Recipient's E-mail
+$to = 'info@designesia.com';  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
 
 $name = $_REQUEST['name'];
@@ -8,22 +8,26 @@ $email = $_REQUEST['email'];
 $phone = $_REQUEST['phone'];
 $msg = $_REQUEST['message'];
 
-$email_from = $name . '<' . $email . '>';
+$email_from = $name.'<'.$email.'>';
 
 $headers = "MIME-Version: 1.1";
 $headers .= "Content-type: text/html; charset=iso-8859-1";
-$headers .= "From: " . $name . '<' . $email . '>' . "\r\n"; // Sender's E-mail
-$headers .= "Return-Path:" . "From:" . $email;
+$headers .= "From: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
+$headers .= "Return-Path:"."From:" . $email;
 
 $message .= 'Name : ' . $name . "\n";
 $message .= 'Email : ' . $email . "\n";
 $message .= 'Phone : ' . $phone . "\n";
 $message .= 'Message : ' . $msg;
 
-if (@mail($to, $subject, $message, $email_from)) {
+if (@mail($to, $subject, $message, $email_from))
+{
 	// Transfer the value 'sent' to ajax function for showing success message.
 	echo 'sent';
-} else {
+}
+else
+{
 	// Transfer the value 'failed' to ajax function for showing error message.
 	echo 'failed';
 }
+?>

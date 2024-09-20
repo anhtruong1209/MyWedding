900618f0b35237af83048b261593aea53d9ea428
TruongNDA <sdtla0911114819@gmail.com>
2024-09-20 10:40:12 +0700

    aaaa

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
     jQuery(function() {
-        jQuery('#defaultCountdown').countdown({ until: new Date(2024, 11, 10, 17) }); // year, month, date, hour
-        jQuery('#defaultCountdownHoi').countdown({ until: new Date(2024, 11, 2, 14) }); // year, month, date, hour
+        jQuery('#defaultCountdown').countdown({ until: new Date(2024, 10, 10, 17) }); // year, month, date, hour
+        jQuery('#defaultCountdownHoi').countdown({ until: new Date(2024, 10, 2, 14) }); // year, month, date, hour
     });
 });

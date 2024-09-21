51936f3035e50c61e6e84a4603cee0b9303ae1b4
TruongNDA <sdtla0911114819@gmail.com>
2024-09-21 10:16:24 +0700

    update

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-btn").addEventListener("click", function() {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("open"); // Toggles the open class to show/hide the menu
+});

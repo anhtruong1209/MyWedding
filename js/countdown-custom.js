6f7f26c51eda7d258e86903d540184276135f6ff
TrươngNDA <truongnd@absoltech.vn>
2024-10-14 09:05:57 +0700

    aaaaa

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,10 @@
-jQuery(document).ready(function() {
-    jQuery(function() {
-        jQuery('#defaultCountdown').countdown({ until: new Date(2024, 10, 10, 17) }); // year, month, date, hour
-        jQuery('#defaultCountdownHoi').countdown({ until: new Date(2024, 10, 2, 14) }); // year, month, date, hour
-    });
+jQuery(document).ready(function () {
+  jQuery(function () {
+    jQuery("#defaultCountdown").countdown({
+      until: new Date(2024, 10, 10, 17),
+    }); // year, month, date, hour
+    jQuery("#defaultCountdownHoi").countdown({
+      until: new Date(2024, 10, 2, 14),
+    }); // year, month, date, hour
+  });
 });
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-document.getElementById("menu-btn").addEventListener("click", function() {
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("open"); // Toggles the open class to show/hide the menu
+document.getElementById("menu-btn").addEventListener("click", function () {
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("open"); // Toggles the open class to show/hide the menu
 });
